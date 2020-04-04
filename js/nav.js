984ba5681af1bb4c574e0d924651069affaf5416
Andrei Lavrenov <andrei.lavrenov@gmail.com>
2020-04-04 07:43:55 +0200

    navigation done 90%
    Some minor issues left with navigation such as colors, performance and content, but it's working as intended and has the right size

--- js/nav.js
+++ js/nav.js
@@ -1,18 +1,6 @@
-// function responsiveNavigation() {
-//     var x = document.getElementById("nav");
-
-//     if (x.className === "topnav") {
-//       x.className += " responsive";
-//     } else {
-//       x.className = "topnav";
-//     }
-// }
-
-function responsiveNavigation() {
-    document.getElementById("sidenav").style.width = "250px";
-  }
-  
-  function closeNav() {
-    document.getElementById("sidenav").style.width = "0";
-}
-
+$(document).ready(function(){
+    $('.c-nav__icon').click(function(){
+        $(this).toggleClass('open');
+        $('.c-sidenav').toggleClass('c-sidenav--visible');
+    });
+});
